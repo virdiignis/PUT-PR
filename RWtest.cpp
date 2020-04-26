5c07d78b2cd21b79c4b11c58b773c53d8ee213ea
virdiignis <gbryk1@gmail.com>
2020-04-27 00:54:46 +0200

    cleanup and modern C++

--- RWtest.cpp
+++ RWtest.cpp
@@ -5,74 +5,68 @@
 #include <algorithm> //for std::remove
 #include <unordered_set>
 #include <cmath>
+#include <set>
 #include "Time.h"
 
-
-void print_out_vector(std::vector<int> vec) {
-	for (std::vector<int>::const_iterator i = vec.begin(); i != vec.end(); ++i)
-		std::cout << *i << ' ';
-}
-
-std::vector<int> first_sequential(int min, int max) {
-
-	std::vector<int> result;
-
-	if (min == 2) {
-		result.push_back(2);
-	}
-
-	for (int i = min; i <= max; i++) {
-		for (int j = 2; j <= ceil(sqrt(i)); j++) {
-			if (i % j == 0) {
-				break;
-			}
-			else if (j == ceil(sqrt(i))) {
-				result.push_back(i);
-			}
-		}
-	}
-
-	return result;
-}
-
 template<typename T>
-void remove_intersection(std::vector<T>& a, std::vector<T>& b) {
-	std::unordered_multiset<T> st;
-	st.insert(a.begin(), a.end());
-	st.insert(b.begin(), b.end());
-	auto predicate = [&st](const T & k) { return st.count(k) > 1; };
-	a.erase(std::remove_if(a.begin(), a.end(), predicate), a.end());
-	b.erase(std::remove_if(b.begin(), b.end(), predicate), b.end());
+void print_out_set(const T &set) {
+    for (int i : set)
+        std::cout << i << ' ';
+    std::cout << std::endl;
+    fflush(stdout);
 }
 
-std::vector<int> second_sequential(int min, int max) {
+std::set<int> first_sequential(int min, int max) {
+    std::set<int> result;
 
-	std::vector<int> result(max-min+1);
-	std::iota(result.begin(), result.end(), min);
-	std::vector<int> primary_numbers = first_sequential(2, ceil(sqrt(max)));
-	std::vector<int> numbers_to_remove;
+    if (min == 2) {
+        result.insert(2);
+    }
 
-	print_out_vector(primary_numbers);
+    for (int i = min; i <= max; i++) {
+        for (int j = 2; j <= ceil(sqrt(i)); j++) {
+            if (i % j == 0) {
+                break;
+            } else if (j == ceil(sqrt(i))) {
+                result.insert(i);
+            }
+        }
+    }
 
-	for (int i = 0; i < result.size(); i++) {
-		for (int j = 0; j < primary_numbers.size(); j++) {
-			if (result[i] % primary_numbers[j] == 0) {
-				numbers_to_remove.push_back(result[i]);
-				break;
-			}
-		}
-	}
+    return result;
+}
 
-	remove_intersection(result, numbers_to_remove);
-	print_out_vector(result);
 
-	return result;
+std::set<int> second_sequential(int min, int max) {
+    std::set<int> initial;
+    for (int i = min; i < max; i++) initial.insert(i);
+    std::set<int> primary_numbers = first_sequential(2, ceil(sqrt(max)));
+    std::set<int> numbers_to_erase;
+
+    print_out_set<std::set<int>>(primary_numbers);
+
+    for (int i : initial) {
+        for (int primary_number : primary_numbers) {
+            if (i % primary_number == 0) {
+                numbers_to_erase.insert(i);
+                break;
+            }
+        }
+    }
+
+    std::set<int> result;
+
+    std::set_difference(initial.begin(), initial.end(), numbers_to_erase.begin(), numbers_to_erase.end(),
+                        inserter(result, result.end()));
+    print_out_set<std::set<int>>(result);
+
+    return result;
 }
 
 
 int main() {
-    //std::vector<int> result = first_sequential(10, 49);
-    //print_out_vector(result);
+    std::set<int> result = second_sequential(2, 49);
+    //print_out_set(result);
 //
 //	int maxThreadsCount = omp_get_max_threads();
 //	int threadsCount = omp_get_num_threads();
